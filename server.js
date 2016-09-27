7aab7a9a90e859fbb1b1c013090894bdea3fe7f3
MathewTheCoder <jmathew1201@live.com>
2016-09-27 18:08:01 +0530

    Added binding to the page

--- server.js
+++ server.js
@@ -5,8 +5,48 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article = {
+	title: 'Article-1|Mathew',
+	heading: 'Article-1',
+	date: '10 Sep,2016',
+	content: `
+		<p>
+			It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+		</p>
+	`
+};
+function createTemplate(data){
+var title = data.title;
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
+var template = `
+<!DOCTYPE html>
+<html>
+<head>
+	<title>{title}</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<meta name="viewport" content="width=device-width, intial-scale=1" />
+</head>
+<body>
+<div class='container'>
+	<div>
+		<h2>${heading}</h2>
+	</div>
+	<div>
+		${date}
+	</div>
+	<div>
+		${content}
+	</div>
+</div>
+</body>
+</html>
+`;
+return template;
+}
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(article));
 });
 
 app.get('/article-two', function (req, res) {

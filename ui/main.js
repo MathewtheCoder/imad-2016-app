ef86166f092153baeb87e86c5ded6825820ed632
MathewTheCoder <jmathew1201@live.com>
2016-09-29 19:40:54 +0530

    Edited the counter logic

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,25 @@
-var counter = 0;
+
 var but = document.getElementById('counter');
-var count = document.getElementById('count');
+
 but.onclick = function() {
 	console.info("Pressed");
 	//Make request object
 	var request = new XMLHttpRequest();
 	//Capture the response and store it in a variable
 	request.onreadystatechange = function() {
-		if(request.readystate === XMLHttpRequest.DONE && request.status == 200){
+		console.warn(request.readystate);
+		if(request.readystate === XMLHttpRequest.DONE){
 			//Do something
-			counter = request.responseText;
-			//Render it in span
-			count.innerHTML = counter.toString();		
+			console.warn(request.status);
+			if(request.status === 200){
+				console.info("Ready");
+				var counter = request.responseText;
+				var count = document.getElementById('count');
+				//Render it in span
+				count.innerHTML = counter.toString();
+			}		
 		}	
 	};
-	request.open("http://mathewthecoder.imad.hasura-app.io/counter", true);
+	request.open('GET',"http://mathewthecoder.imad.hasura-app.io//counter", true);
 	request.send(null);
 };

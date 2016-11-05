4999676536ffc903675c723c9167cacc64439b29
MathewTheCoder <jmathew1201@live.com>
2016-11-05 22:35:49 +0530

    Added js code for scroll animations

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	
+	
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
@@ -15,6 +16,37 @@ $(document).ready(function(){
 		return false;
 	});
 	
+	
+	//Contact Us Animations
+	//Cache reference to window and animation items
+	var $animation_elements = $('.animation-element');
+	var $window = $(window);
+
+	function check_if_in_view() {
+	  var window_height = $window.height();
+	  var window_top_position = $window.scrollTop();
+	  var window_bottom_position = (window_top_position + window_height);
+	 
+	  $.each($animation_elements, function() {
+	    var $element = $(this);
+	    var element_height = $element.outerHeight();
+	    var element_top_position = $element.offset().top;
+	    var element_bottom_position = (element_top_position + element_height);
+	 
+	    //check to see if this current container is within viewport
+	    if ((element_bottom_position >= window_top_position) &&
+	        (element_top_position <= window_bottom_position)) {
+	      $element.addClass('in-view');
+	    } else {
+	      $element.removeClass('in-view');
+	    }
+	  });
+	}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+	}
+
 });
 /*
 var but = document.getElementById('counter');

1d3148c8e866f50b87937d358604f0d5833174af
MathewTheCoder <jmathew1201@live.com>
2016-11-05 19:23:05 +0530

    Added scroll to top Button

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,22 @@
-
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
+/*
 var but = document.getElementById('counter');
 
 but.onclick = function() {
@@ -54,3 +72,4 @@ request.open('GET',"http://mathewthecoder.imad.hasura-app.io/search-name?name="+
 request.send(null);
 
 };
+*/

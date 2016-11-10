919f440dc16fbdb0b8d210d2cf2124ca837a90c8
MathewTheCoder <jmathew1201@live.com>
2016-11-10 19:41:00 +0530

    Added smooth scroll and header image

--- server.js
+++ server.js
@@ -147,7 +147,9 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/Arrow-Up.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Arrow-Up.png'));
 });
-
+app.get('/ui/header.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'header.jpg'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
--- ui/main.js
+++ ui/main.js
@@ -17,91 +17,16 @@ $(document).ready(function(){
 	});
 	
 	
-	//Contact Us Animations
-	//Cache reference to window and animation items
-	var $animation_elements = $('.animation-element');
-	var $window = $(window);
-
-	function check_if_in_view() {
-	  var window_height = $window.height();
-	  var window_top_position = $window.scrollTop();
-	  var window_bottom_position = (window_top_position + window_height);
-	 
-	  $.each($animation_elements, function() {
-	    var $element = $(this);
-	    var element_height = $element.outerHeight();
-	    var element_top_position = $element.offset().top;
-	    var element_bottom_position = (element_top_position + element_height);
-	 
-	    //check to see if this current container is within viewport
-	    if ((element_bottom_position >= window_top_position) &&
-	        (element_top_position <= window_bottom_position)) {
-	      $element.addClass('in-view');
-	    } else {
-	      $element.removeClass('in-view');
-	    }
-	  });
-	}
-
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
-	}
-
+	$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
-/*
-var but = document.getElementById('counter');
-
-but.onclick = function() {
-	
-	//Make request object
-	var request = new XMLHttpRequest();
-	//Capture the response and store it in a variable
-	request.onreadystatechange = function() {
-		//console.warn(request.readystate);
-		if(request.readyState === XMLHttpRequest.DONE){
-			//Do something
-			if(request.status === 200){
-				//console.info("Ready");
-				var counter = request.responseText;
-				var count = document.getElementById('count');
-				//Render it in span
-				count.innerHTML = counter.toString();
-			}		
-		}	
-	};
-	request.open('GET',"http://mathewthecoder.imad.hasura-app.io/counter", true);
-	request.send(null);
-};
-var inputField = document.getElementById('txt_name');
-var submit = document.getElementById('btn_submit');
-submit.onclick = function() {
-	//Extract text
-	var input = inputField.value;
-	//Make request object
-	var request = new XMLHttpRequest();
-	//Capture the response and store it in a variable
-	request.onreadystatechange = function() {
-		//console.warn(request.readystate);
-		if(request.readyState === XMLHttpRequest.DONE){
-			//Do something
-			if(request.status === 200){
-				//console.info("Ready");
-				var names = request.responseText;
-				names = JSON.parse(names);
-				var list = '';
-				for (var i=0; i<names.length; i++){
-					list+="<li>"+names[i]+"</li>";
-				}
-				var ul = document.getElementById('nameList');
-				ul.innerHTML = list;
-			}		
-		}
-//Make a request to server and send the name
-
-};
-//Send request
-request.open('GET',"http://mathewthecoder.imad.hasura-app.io/search-name?name="+input, true);
-request.send(null);
-
-};
-*/
